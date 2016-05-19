c37f09691f18b57c926fe7a21914b5358d0fa506
Nathan Arnold <soupbadger@gmail.com>
2016-05-19 10:08:33 -0400

    third and final commit

--- problem_1.js
+++ problem_1.js
@@ -3,7 +3,7 @@ var sum = 0;
 var x = 3;
 var y = 5;
 //Run for loop to check for remainder of 0 for numbers 1 - 1000
-for(var i=1; i<=999; i+=1){
+for(var i=1; i<1000; i+=1){
   //nest if statement to check modulus
   if(i % x === 0 || i % y === 0) {
   //add numbers to sum
