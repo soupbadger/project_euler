b072eaf2f25e03135fc3dfc94fc792c774405aa1
Nathan Arnold <soupbadger@gmail.com>
2016-05-19 10:05:36 -0400

    second commit

--- problem_1.js
+++ problem_1.js
@@ -3,12 +3,12 @@ var sum = 0;
 var x = 3;
 var y = 5;
 //Run for loop to check for remainder of 0 for numbers 1 - 1000
-for(var i=1; i<=1000; i+=1){
+for(var i=1; i<=999; i+=1){
   //nest if statement to check modulus
   if(i % x === 0 || i % y === 0) {
   //add numbers to sum
   sum += i;
   }
 }
-//print final sum of numbers between 1 and 1000 which divide evenly by 3 or 5
+//print final sum of natural numbers between 1 and 1000 which divide evenly by 3 or 5
 console.log(sum);
