9ceee5cc6f00e045d8a6b9cfb40fe111bd2486ab
Nathan Arnold <soupbadger@gmail.com>
2016-05-21 22:39:19 -0400

    commit

--- problem_3.js
+++ problem_3.js
@@ -5,3 +5,11 @@ What is the largest prime factor of the number 600851475143 ?
 */
 
 var x = 0;
+var number = 600851475143;
+
+for(var i = 0; i <= number; i += 1) {
+  if(number % i != 0){
+    x = i;
+  }
+}
+console.log(x);
