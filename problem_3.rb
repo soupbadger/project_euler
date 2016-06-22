5ae224499fbe82e704ecb34ebe9856bf8cb81f7f
Nathan Arnold <soupbadger@gmail.com>
2016-06-22 18:08:16 -0400

    decreased variables; still doesn't work

--- problem_3.rb
+++ problem_3.rb
@@ -1,16 +1,13 @@
-=begin
+
 
 x = 0
-number = 600851475143
+number = 100000
 
 for i in 1...number
   if number % i != 0
     x = i
   end
+  puts x
 end
-
-puts x
-
-=end
 
 #This program does not work. Will need to advance in Ruby skills first
