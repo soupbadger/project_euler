5901daffcd6d3eaff9cc8a5aff00b0fcbecdd5e7
Nathan Arnold <soupbadger@gmail.com>
2016-06-22 18:09:16 -0400

    test commit for Atom

--- problem_3.rb
+++ problem_3.rb
@@ -1,4 +1,4 @@
-
+#This program does not work. Will need to advance in Ruby skills first
 
 x = 0
 number = 100000
@@ -7,7 +7,6 @@ for i in 1...number
   if number % i != 0
     x = i
   end
-  puts x
 end
 
-#This program does not work. Will need to advance in Ruby skills first
+puts x
